3b9fe0b6389a0dd9415c6fd802a22d926a9d2b4b
chenmo <wwwchenyuhan@qq.com>
2016-08-19 16:29:06 +0800

    目录的深度优先遍历

---
+++ scanfdir/printf.c
@@ -0,0 +1,15 @@
+ ///
+ /// @file    printf.c
+ /// @author  Chenmo(mars_chenmo@sina.com)
+ /// @date    2016-08-17 21:25:35
+ ///
+ 
+#include <stdio.h>
+
+int main()
+{
+     // %*s   5个空格
+    printf("%*s%s\n",5," ","hello");  
+
+    return 0;
+}
